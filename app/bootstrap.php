f203b4513e7190d8646c85040b7dfb37c324858e
Christopher <mohalebrown@gmail.com>
2026-06-30 08:51:34 +0200

    Revert "Add SQLite local setup"
    This reverts commit 00464299d653b4401698c9c5c15d2a890638ae18.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -39,5 +39,5 @@ if (($GLOBALS['config']['app']['debug'] ?? false) === true) {
     error_reporting(E_ALL);
 } else {
     ini_set('display_errors', '0');
-    error_reporting(E_ALL & ~E_DEPRECATED);
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 }
